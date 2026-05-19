8d38a23ab15bc76a241838fa90e5ccea17a00551
Marisa do Nascimento Cruz <marisanascimento143@gmail.com>
2026-05-19 19:18:07 -0300

    Atualização da ineterface que possibilita filtro por data

--- Interface.py
+++ Interface.py
@@ -4,208 +4,345 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# =========================================================
+# CONEXÃO
+# =========================================================
+
 engine = create_engine(
     "postgresql+psycopg2://postgres:23062006@localhost:5432/Mfix_BD"
 )
 
-def consultar(query, params={}):
+# =========================================================
+# QUERY
+# =========================================================
+
+def query_db(query, params={}):
+
     with engine.connect() as conn:
-        result = conn.execute(text(query), params)
-        df = pd.DataFrame(result.fetchall(), columns=result.keys())
+
+        result = conn.execute(
+            text(query),
+            params
+        )
+
+        df = pd.DataFrame(
+            result.fetchall(),
+            columns=result.keys()
+        )
+
     return df.to_dict(orient="records")
 
+# =========================================================
+# HOME
+# =========================================================
 
-# =========================
-# INTERFACE DW
-# =========================
+@app.route("/", methods=["GET", "POST"])
+def home():
 
-@app.route("/", methods=["GET"])
-def interface_dw():
+    produto = request.form.get(
+        "produto", ""
+    ).strip()
 
-    produto = request.args.get("produto", "")
-    tipo = request.args.get("tipo", "")
-    fornecedor = request.args.get("fornecedor", "")
+    fornecedor = request.form.get(
+        "fornecedor", ""
+    ).strip()
 
-    filtros = []
-    params = {}
+    movimentacao = request.form.get(
+        "movimentacao", ""
+    ).strip()
 
-    if produto:
-        filtros.append("p.descricao_prod ILIKE :produto")
-        params["produto"] = f"%{produto}%"
+    data_inicial = request.form.get(
+        "data_inicial", ""
+    ).strip()
 
-    if tipo:
-        filtros.append("tm.descricao_mov = :tipo")
-        params["tipo"] = tipo
+    data_final = request.form.get(
+        "data_final", ""
+    ).strip()
 
-    if fornecedor:
-        filtros.append("forn.nome_forn ILIKE :fornecedor")
-        params["fornecedor"] = f"%{fornecedor}%"
+    query = """
 
-    where = ""
+        SELECT
 
-    if filtros:
-        where = "WHERE " + " AND ".join(filtros)
+            dp.descricao_prod,
+            dp.categoria_prod,
+            dp.procedencia_prod,
 
-    query = f"""
-    SELECT
-        p.descricao_prod,
-        p.categoria_prod,
-        p.procedencia_prod,
+            df.nome_forn,
 
-        forn.nome_forn,
+            tm.descricao_mov,
 
-        l.numero_lote,
-        l.data_validade_lote,
+            dt.nome_transp,
 
-        tm.descricao_mov,
+            dl.numero_lote,
+            dl.data_validade_lote,
 
-        fm.quantidade,
-        fm.valor_total,
+            fm.quantidade,
+            fm.valor_total,
 
-        t.data,
+            fm.data,
 
-        tr.nome_transp
+            (
 
-    FROM fato_movimentacao fm
+                SELECT MAX(f2.quantidade)
 
-    JOIN dim_produto p
-    ON fm.id_produto = p.id_produto
+                FROM fato_movimentacao f2
 
-    JOIN dim_fornecedor forn
-    ON fm.id_fornecedor = forn.id_fornecedor
+                WHERE
+                    f2.id_produto = fm.id_produto
+                    AND f2.id_tipo_mov = 2
 
-    JOIN dim_lote l
-    ON fm.id_lote = l.id_lote
+            ) AS melhor_venda
 
-    JOIN dim_tipo_mov tm
-    ON fm.id_tipo_mov = tm.id_tipo_mov
+        FROM fato_movimentacao fm
 
-    JOIN dim_tempo t
-    ON fm.data = t.data
+        JOIN dim_produto dp
+        ON fm.id_produto = dp.id_produto
 
-    JOIN dim_transportadora tr
-    ON fm.id_transp = tr.id_transp
+        JOIN dim_fornecedor df
+        ON fm.id_fornecedor = df.id_fornecedor
 
-    {where}
+        JOIN dim_tipo_mov tm
+        ON fm.id_tipo_mov = tm.id_tipo_mov
 
-    ORDER BY t.data DESC
-    LIMIT 50
+        JOIN dim_transportadora dt
+        ON fm.id_transp = dt.id_transp
+
+        JOIN dim_lote dl
+        ON fm.id_lote = dl.id_lote
+
+        WHERE 1=1
+
     """
 
-    dados = consultar(query, params)
+    params = {}
 
-    return render_template("index.html", dados=dados)
+    # =====================================================
+    # PRODUTO
+    # =====================================================
 
+    if produto != "":
 
-# =========================
+        query += """
+
+            AND LOWER(dp.descricao_prod)
+
+            LIKE LOWER(:produto)
+
+        """
+
+        params["produto"] = f"%{produto}%"
+
+    # =====================================================
+    # FORNECEDOR
+    # =====================================================
+
+    if fornecedor != "":
+
+        query += """
+
+            AND LOWER(df.nome_forn)
+
+            LIKE LOWER(:fornecedor)
+
+        """
+
+        params["fornecedor"] = f"%{fornecedor}%"
+
+    # =====================================================
+    # MOVIMENTAÇÃO
+    # =====================================================
+
+    if movimentacao != "":
+
+        query += """
+
+            AND CAST(fm.id_tipo_mov AS TEXT)
+            = :movimentacao
+
+        """
+
+        params["movimentacao"] = str(movimentacao)
+
+    # =====================================================
+    # DATA INICIAL
+    # =====================================================
+
+    if data_inicial != "":
+
+        query += """
+
+            AND fm.data >= :data_inicial
+
+        """
+
+        params["data_inicial"] = data_inicial
+
+    # =====================================================
+    # DATA FINAL
+    # =====================================================
+
+    if data_final != "":
+
+        query += """
+
+            AND fm.data <= :data_final
+
+        """
+
+        params["data_final"] = data_final
+
+    # =====================================================
+    # FINAL QUERY
+    # =====================================================
+
+    query += """
+
+        ORDER BY fm.data DESC
+
+        LIMIT 60
+
+    """
+
+    dados = query_db(
+        query,
+        params
+    )
+
+    return render_template(
+        "index.html",
+        dados=dados
+    )
+
+# =========================================================
+# AUTOCOMPLETE PRODUTO
+# =========================================================
+
+@app.route("/autocomplete_produto")
+def autocomplete_produto():
+
+    termo = request.args.get(
+        "term", ""
+    )
+
+    produtos = query_db("""
+
+        SELECT DISTINCT
+
+            descricao_prod
+
+        FROM dim_produto
+
+        WHERE LOWER(descricao_prod)
+
+        LIKE LOWER(:termo)
+
+        ORDER BY descricao_prod
+
+        LIMIT 10
+
+    """, {
+
+        "termo": f"%{termo}%"
+
+    })
+
+    return jsonify(produtos)
+
+# =========================================================
+# AUTOCOMPLETE FORNECEDOR
+# =========================================================
+
+@app.route("/autocomplete_fornecedor")
+def autocomplete_fornecedor():
+
+    termo = request.args.get(
+        "term", ""
+    )
+
+    fornecedores = query_db("""
+
+        SELECT DISTINCT
+
+            nome_forn
+
+        FROM dim_fornecedor
+
+        WHERE LOWER(nome_forn)
+
+        LIKE LOWER(:termo)
+
+        ORDER BY nome_forn
+
+        LIMIT 10
+
+    """, {
+
+        "termo": f"%{termo}%"
+
+    })
+
+    return jsonify(fornecedores)
+
+# =========================================================
 # DASHBOARD
-# =========================
+# =========================================================
 
 @app.route("/dashboard")
 def dashboard():
 
-    kpis = consultar("""
-    SELECT
-        COUNT(*) AS total_mov,
-        SUM(valor_total) AS receita,
-        SUM(quantidade) AS quantidade_total,
-        COUNT(DISTINCT id_produto) AS produtos
-    FROM fato_movimentacao
-    """)
+    kpis = query_db("""
 
-    top_produtos = consultar("""
-    SELECT
-        p.descricao_prod,
-        SUM(fm.quantidade) AS total
+        SELECT
 
-    FROM fato_movimentacao fm
+            COALESCE(
+                SUM(valor_total),0
+            ) AS valor_total,
 
-    JOIN dim_produto p
-    ON fm.id_produto = p.id_produto
+            COALESCE(
+                SUM(quantidade),0
+            ) AS quantidade_total,
 
-    GROUP BY p.descricao_prod
+            COUNT(*) FILTER(
+                WHERE id_tipo_mov = 1
+            ) AS entradas,
 
-    ORDER BY total DESC
-    LIMIT 5
-    """)
+            COUNT(*) FILTER(
+                WHERE id_tipo_mov = 2
+            ) AS saidas,
 
-    entrada_saida = consultar("""
-    SELECT
-        tm.descricao_mov,
-        SUM(fm.quantidade) AS total
+            COUNT(*) FILTER(
+                WHERE id_tipo_mov = 3
+            ) AS ajustes,
 
-    FROM fato_movimentacao fm
+            ROUND(
+                AVG(valor_total),2
+            ) AS ticket_medio,
 
-    JOIN dim_tipo_mov tm
-    ON fm.id_tipo_mov = tm.id_tipo_mov
+            COUNT(DISTINCT id_produto)
+            AS produtos_ativos,
 
-    GROUP BY tm.descricao_mov
+            SUM(quantidade)
+            AS estoque_atual,
+
+            COUNT(*)
+            AS total_movimentacoes,
+
+            COUNT(DISTINCT id_fornecedor)
+            AS total_fornecedores
+
+        FROM fato_movimentacao
+
     """)
 
     return render_template(
         "dashboard.html",
-        kpis=kpis,
-        top_produtos=top_produtos,
-        entrada_saida=entrada_saida
+        kpis=kpis
     )
 
-
-# =========================
-# MODAL DASHBOARD
-# =========================
-
-@app.route("/produto/<nome>")
-def detalhes_produto(nome):
-
-    query = """
-    SELECT
-        p.descricao_prod,
-        p.categoria_prod,
-        p.procedencia_prod,
-
-        forn.nome_forn,
-
-        l.numero_lote,
-        l.data_validade_lote,
-
-        tm.descricao_mov,
-
-        fm.quantidade,
-        fm.valor_total,
-
-        t.data,
-
-        tr.nome_transp
-
-    FROM fato_movimentacao fm
-
-    JOIN dim_produto p
-    ON fm.id_produto = p.id_produto
-
-    JOIN dim_fornecedor forn
-    ON fm.id_fornecedor = forn.id_fornecedor
-
-    JOIN dim_lote l
-    ON fm.id_lote = l.id_lote
-
-    JOIN dim_tipo_mov tm
-    ON fm.id_tipo_mov = tm.id_tipo_mov
-
-    JOIN dim_tempo t
-    ON fm.data = t.data
-
-    JOIN dim_transportadora tr
-    ON fm.id_transp = tr.id_transp
-
-    WHERE p.descricao_prod = :nome
-
-    ORDER BY t.data DESC
-    """
-
-    dados = consultar(query, {"nome": nome})
-
-    return jsonify(dados)
-
+# =========================================================
+# START
+# =========================================================
 
 if __name__ == "__main__":
+
     app.run(debug=True)
